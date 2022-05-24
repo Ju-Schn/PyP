e94e062c3fd0d3b88548084c25033eb0a0bf2b7b
Julia Schneider <julia_schneider_mail@posteo.de>
2022-05-24 17:57:42 +0200

    add button variant submit

--- client/src/components/StyledButton.js
+++ client/src/components/StyledButton.js
@@ -1,8 +1,16 @@
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
 
 export default styled.button`
   border-radius: 30px;
-  background-color: #93d94e;
+  background-color: #223240;
+  color: #93d94e;
   padding: 8px 16px;
   font-weight: 600;
+
+  ${props =>
+    props.variant === 'submit' &&
+    css`
+      background-color: #93d94e;
+      color: #223240;
+    `}
 `
