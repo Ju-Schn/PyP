82b406192fecf30e639fde681ed2f08447a96866
Julia Schneider <julia_schneider_mail@posteo.de>
2022-05-24 17:04:27 +0200

    add upload

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,13 @@
 import { useEffect, useState } from 'react'
 import styled from 'styled-components'
+import axios from 'axios'
 import getImages from './api'
+import StyledButton from './components/StyledButton'
 
 function App() {
   const [imageList, setImageList] = useState([])
   const [nextCursor, setNextCursor] = useState(null)
+  const [previewSource, setPreviewSource] = useState('')
 
   useEffect(() => {
     const fetchData = async () => {
@@ -16,17 +19,30 @@ function App() {
     fetchData()
   }, [])
 
-  async function handleLoadMoreButtonClick() {
-    const responseJson = await getImages(nextCursor);
-    setImageList(currentImageList => [
-      ...currentImageList,
-      ...responseJson.resources,
-    ])
-    setNextCursor(responseJson.next_cursor)
-  }
-
   return (
     <>
+      <div>
+        <form onSubmit={handleSubmitFile}>
+          <input
+            type="file"
+            name="image"
+            accept="image/*"
+            onChange={handleFileInputChange}
+          />
+          <StyledButton>upload</StyledButton>
+        </form>
+      </div>
+      {previewSource && (
+        <>
+          <span>Preview</span>
+          <img
+            src={previewSource}
+            alt="chosen"
+            style={{ height: '300px', display: 'block' }}
+          />
+        </>
+      )}
+      <StyledButton onClick={synchronizeData}>synchronize</StyledButton>
       <GridContainer>
         {imageList.map(image => (
           <img key={image.asset_id} src={image.url} alt={image.publicId} />
@@ -34,17 +50,63 @@ function App() {
       </GridContainer>
       <div>
         {nextCursor && (
-          <button onClick={handleLoadMoreButtonClick}>Load More</button>
+          <StyledButton onClick={handleLoadMoreButtonClick}>
+            Load More
+          </StyledButton>
         )}
       </div>
     </>
   )
+
+  async function handleLoadMoreButtonClick() {
+    const responseJson = await getImages(nextCursor)
+    setImageList(currentImageList => [
+      ...currentImageList,
+      ...responseJson.resources,
+    ])
+    setNextCursor(responseJson.next_cursor)
+  }
+
+  function handleFileInputChange(event) {
+    const file = event.target.files[0]
+    previewFile(file)
+  }
+
+  function previewFile(file) {
+    const reader = new FileReader()
+    reader.readAsDataURL(file)
+    reader.onloadend = () => {
+      setPreviewSource(reader.result)
+    }
+  }
+
+  function handleSubmitFile(event) {
+    event.preventDefault()
+    if (!previewSource) return
+    uploadImage()
+  }
+
+  function uploadImage() {
+    console.log(previewSource)
+    const formData = new FormData()
+    formData.append('file', previewSource)
+    formData.append('upload_preset', 'rkqvotof')
+
+    axios.post(
+      'https://api.cloudinary.com/v1_1/dkti3sjec/image/upload/',
+      formData
+    )
+  }
+
+  function synchronizeData() {
+    window.location.reload()
+  }
 }
 
 const GridContainer = styled.div`
   display: grid;
   gap: 8px;
-  grid-template-columns: repeat(auto-fill, minmax(450px, 1fr));
+  grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
 
   img {
     width: 100%;
