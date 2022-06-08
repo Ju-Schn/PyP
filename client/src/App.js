277b02aeb072c66270a1693dba9aa9b6605d32e4
Julia Schneider <julia_schneider_mail@posteo.de>
2022-06-08 12:54:27 +0200

    add toast contatiner

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,31 @@
+import 'react-toastify/dist/ReactToastify.css'
+
 import Upload from './pages/Upload';
 import Gallery from './pages/Gallery';
 
 import { Routes, Route } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
 
 function App() {
-  return (
+  return (<>
     <Routes>
       <Route path="/" element={<Gallery />} />
       <Route path="/upload" element={<Upload />} />
     </Routes>
+    <ToastContainer
+    position='top-center'
+    autoClose={2500}
+    hideProgressBar={true}
+    newestOnTop={false}
+    closeOnClick
+    rtl={false}
+    pauseOnFocusLoss
+    draggable
+    pauseOnHover
+    theme='colored'
+    role='alert'
+     />
+     </>
   );
 }
 
