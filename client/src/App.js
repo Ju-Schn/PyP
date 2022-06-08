ca8064279525ca6c0e8f9a6c12e66f468607bfe2
Julia Schneider <julia_schneider_mail@posteo.de>
2022-06-08 13:19:02 +0200

    update App

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import 'react-toastify/dist/ReactToastify.css'
+import 'react-toastify/dist/ReactToastify.css';
 
 import Upload from './pages/Upload';
 import Gallery from './pages/Gallery';
@@ -7,25 +7,26 @@ import { Routes, Route } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 
 function App() {
-  return (<>
-    <Routes>
-      <Route path="/" element={<Gallery />} />
-      <Route path="/upload" element={<Upload />} />
-    </Routes>
-    <ToastContainer
-    position='top-center'
-    autoClose={2500}
-    hideProgressBar={true}
-    newestOnTop={false}
-    closeOnClick
-    rtl={false}
-    pauseOnFocusLoss
-    draggable
-    pauseOnHover
-    theme='colored'
-    role='alert'
-     />
-     </>
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Gallery />} />
+        <Route path="/upload" element={<Upload />} />
+      </Routes>
+      <ToastContainer
+        position="top-center"
+        autoClose={2500}
+        hideProgressBar={true}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="colored"
+        role="alert"
+      />
+    </>
   );
 }
 
