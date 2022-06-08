81287f3b92a22d614bd8bc848fb0d38f239c65f5
Julia Schneider <julia_schneider_mail@posteo.de>
2022-06-08 12:15:36 +0200

    update tests

--- client/src/pages/Gallery.spec.js
+++ client/src/pages/Gallery.spec.js
@@ -1,24 +1,26 @@
-import {MemoryRouter} from 'react-router-dom'
-import { render, screen } from '@testing-library/react'
+import { MemoryRouter } from 'react-router-dom';
+import { render, screen } from '@testing-library/react';
 
-import Gallery from './Gallery'
-
+import Gallery from './Gallery';
 
 describe('Gallery', () => {
-    it('renders one heading, two buttons and two navigation links', () => {
-        render(
-            <MemoryRouter>
-                <Gallery />
-            </MemoryRouter>
-        );
+  it('renders one heading, two buttons(one filter button and one reset filter button) and two navigation links', () => {
+    render(
+      <MemoryRouter>
+        <Gallery />
+      </MemoryRouter>
+    );
 
-        const allHeadings = screen.getAllByRole('heading')
-        const allButtons = screen.getAllByRole('button')
-        const navigationLinks = screen.getAllByRole('link');
+    const allHeadings = screen.getAllByRole('heading');
+    const allButtons = screen.getAllByRole('button');
+    const navigationLinks = screen.getAllByRole('link');
+    const filterButton = screen.getByRole('button', { name: /filter/i });
+    const resetFilterButton = screen.getByRole('button', { name: /reset/i });
 
-        expect(allHeadings).toHaveLength(1)
-        expect(allButtons).toHaveLength(2);
-        expect(navigationLinks).toHaveLength(2);
-
-    })
-})
+    expect(allHeadings).toHaveLength(1);
+    expect(allButtons).toHaveLength(2);
+    expect(navigationLinks).toHaveLength(2);
+    expect(filterButton).toBeInTheDocument();
+    expect(resetFilterButton).toBeInTheDocument();
+  });
+});
